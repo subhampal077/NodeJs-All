8309c735512f69ea0821b2a664aab27e74d3178d
Subham Pal <32848988+subhampal077@users.noreply.github.com>
2025-02-19 20:12:16 +0530

    Delete NodeJs-All directory

--- NodeJs-All/nodejs-apis-master/nodejs-apis-master/server.js
+++
@@ -1,9 +0,0 @@
-import { app } from "./app.js";
-import { connectDb } from "./data/database.js";
-
-
-connectDb;
-console.log(process.env.PORT);
-app.listen(process.env.PORT, () => {
-        console.log(`server is working on ${process.env.PORT}`);
-});
--- NodeJs-All/nodejs-march-master/nodejs-2024-march-master/thirdParty.js
+++
@@ -1,6 +0,0 @@
-import pokemon from "pokemon";
-
-const a = pokemon.all();
-console.log(a);
-
-console.log(pokemon.random());
